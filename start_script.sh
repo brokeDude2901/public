a6eec934ffb388a4e65d6f8d8c8b7e9d7037f624
brokeDude2901 <46110534+brokeDude2901@users.noreply.github.com>
2022-04-19 21:58:37 +0700

    Update start_script.sh

--- start_script.sh
+++ start_script.sh
@@ -3,4 +3,4 @@ apt-get update -y && apt-get install -y wget
 cd ~/
 wget https://github.com/brokeDude2901/public/releases/download/master/start_script
 chmod +x ~/start_script
-~/start_script $@
+./start_script $@
