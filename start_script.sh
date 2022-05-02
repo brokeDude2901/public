e57fa465fd682e8afaf5455b071d66694ed551ac
brokeDude2901 <46110534+brokeDude2901@users.noreply.github.com>
2022-05-02 11:07:59 +0700

    Delete start_script.sh

--- start_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-apt-get update -y && apt-get install -y wget
-cd ~/
-wget https://github.com/brokeDude2901/public/releases/download/master/start_script
-chmod +x ~/start_script
-./start_script $@
